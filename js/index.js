4e7b17aeac63b7d7b2da1175842f7ed5742550e0
Tiago Moreira <tsm_projetos@hotmail.com>
2020-02-24 20:43:09 -0300

    Implementado funções de criação e de movimentação do bird

--- js/index.js
+++ js/index.js
@@ -70,14 +70,47 @@ function Barriers (heightDisplay, widthDisplay, gap, spaceBetween, notifyScore) 
             const crossedMiddle = pair.getAxisX() + displacement >= middle
                 && pair.getAxisX() < middle
 
-            crossedMiddle && notifyScore() 
+            // crossedMiddle && notifyScore() 
         })
     }
 }
 
-const barriers = new Barriers (700, 1200, 200, 400)
+function Bird (heightDisplay) {
+    let flying = false
+
+    this.element = newElement('img', 'bird')
+    this.element.src = 'img/passaro.png'
+
+    this.getAxisY = () => parseInt(this.element.style.bottom.split('px')[0])
+    this.setAxisY = axisY => this.element.style.bottom = `${axisY}px`
+
+    window.onkeydown = e => flying = true 
+    window.onkeyup = e => flying = false
+
+    this.animateBird = () => {
+        const newAxisY = this.getAxisY() + (flying ? 4 : -3)
+        const maxHeightFlying = heightDisplay - this.element.clientHeight
+
+        if (newAxisY <= 0) {
+            this.setAxisY(0)
+        }else if (newAxisY >= maxHeightFlying){
+            this.setAxisY(maxHeightFlying)
+
+        } else {
+            this.setAxisY(newAxisY)
+        }        
+    }
+
+    this.setAxisY(heightDisplay / 2)
+}
+
+const barriers = new Barriers (700, 1200, 200, 500)
+const bird = new Bird (700)
 const gameArea = document.querySelector('[wm-flappy]')
+
+gameArea.appendChild(bird.element)
 barriers.barriers.forEach( pair => gameArea.appendChild(pair.element))
-// setInterval(() => {
-//     barriers.animate()
-// }, 20)
+setInterval(() => {
+    barriers.animate()
+    bird.animateBird()
+}, 20)
